af73208d69424d72edd6dddb21528d14071d6a97
Boyang <845657927@qq.com>
2021-09-12 13:04:44 +0800

    remove queryDsl

--- common/src/main/java/com/boyang/landsense/common/repo/LandSenseRepo.java
+++ common/src/main/java/com/boyang/landsense/common/repo/LandSenseRepo.java
@@ -2,9 +2,8 @@ package com.boyang.landsense.common.repo;
 
 import com.boyang.landsense.common.entity.LandSense;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LandSenseRepo extends MongoRepository<LandSense, String>, QuerydslPredicateExecutor<LandSense> {
+public interface LandSenseRepo extends MongoRepository<LandSense, String>/*, QuerydslPredicateExecutor<LandSense>*/ {
 }
